dc58d661cd7c7677044e4b168b2197d237d01b77
Ivan Krasin <imkrasin@gmail.com>
2011-12-15 16:34:27 -0800

    Promote public type declarations and functions to the top of the file

--- reader.go
+++ reader.go
@@ -22,89 +22,6 @@ const (
 	tagCompound  = 10
 )
 
-type nbtReader struct {
-	r *bufio.Reader
-}
-
-func newNbtReader(r io.Reader) (nr *nbtReader, err os.Error) {
-	var rd io.Reader
-	if rd, err = gzip.NewReader(r); err != nil {
-		return
-	}
-	return &nbtReader{r: bufio.NewReader(rd)}, nil
-}
-
-func (r *nbtReader) ReadString() (str string, err os.Error) {
-	var l int
-	if l, err = r.ReadShort(); err != nil {
-		return
-	}
-	data := make([]byte, l)
-	if _, err = io.ReadFull(r.r, data); err != nil {
-		return
-	}
-	return string(data), nil
-}
-
-func (r *nbtReader) ReadShort() (val int, err os.Error) {
-	buf := [2]byte{}
-	if _, err = io.ReadFull(r.r, buf[:]); err != nil {
-		return
-	}
-	val = int(buf[1]) + (int(buf[0]) << 8) // Big Endian
-	return
-}
-
-func (r *nbtReader) ReadInt() (val int, err os.Error) {
-	buf := [4]byte{}
-	if _, err = io.ReadFull(r.r, buf[:]); err != nil {
-		return
-	}
-	for i := 0; i < 4; i++ {
-		val <<= 8
-		val += int(buf[i])
-	}
-	return
-}
-
-func (r *nbtReader) ReadTagTyp() (typ byte, err os.Error) {
-	typ, err = r.r.ReadByte()
-	return
-}
-
-func (r *nbtReader) ReadTagName() (typ byte, name string, err os.Error) {
-	if typ, err = r.r.ReadByte(); err != nil {
-		return
-	}
-	if typ == tagEnd {
-		return
-	}
-	name, err = r.ReadString()
-	return
-}
-
-func (r *nbtReader) ReadByteArray() (data []byte, err os.Error) {
-	var l int
-	if l, err = r.ReadInt(); err != nil {
-		return
-	}
-	data = make([]byte, l)
-	_, err = io.ReadFull(r.r, data)
-	return
-}
-
-type schematicReader struct {
-	r *nbtReader
-}
-
-func newSchematicReader(r io.Reader) (sr *schematicReader, err os.Error) {
-	var nr *nbtReader
-	if nr, err = newNbtReader(r); err != nil {
-		return
-	}
-	return &schematicReader{r: nr}, nil
-}
-
 type Entity struct {
 	Id string
 }
@@ -120,6 +37,47 @@ type Schematic struct {
 	Blocks    []byte
 	Data      []byte
 	Entities  []Entity
+}
+
+func ReadSchematic(input io.Reader) (vol *Schematic, err os.Error) {
+	var r *schematicReader
+	if r, err = newSchematicReader(input); err != nil {
+		return
+	}
+	vol, err = r.Parse()
+	return
+}
+
+func (s *Schematic) XLen() int {
+	return s.Width
+}
+
+func (s *Schematic) YLen() int {
+	return s.Height
+}
+
+func (s *Schematic) ZLen() int {
+	return s.Length
+}
+
+func (s *Schematic) Get(x, y, z int) bool {
+	if x < 0 || y < 0 || z < 0 || x >= s.XLen() || y >= s.YLen() || z >= s.ZLen() {
+		return false
+	}
+	index := y*s.XLen()*s.ZLen() + z*s.XLen() + x
+	return s.Blocks[index] != 0
+}
+
+type schematicReader struct {
+	r *nbtReader
+}
+
+func newSchematicReader(r io.Reader) (sr *schematicReader, err os.Error) {
+	var nr *nbtReader
+	if nr, err = newNbtReader(r); err != nil {
+		return
+	}
+	return &schematicReader{r: nr}, nil
 }
 
 func (r *schematicReader) ReadEntity() (entity Entity, err os.Error) {
@@ -215,31 +173,73 @@ func (r *schematicReader) Parse() (s *Schematic, err os.Error) {
 	return
 }
 
-func (s *Schematic) XLen() int {
-	return s.Width
+type nbtReader struct {
+	r *bufio.Reader
 }
 
-func (s *Schematic) YLen() int {
-	return s.Height
-}
-
-func (s *Schematic) ZLen() int {
-	return s.Length
-}
-
-func (s *Schematic) Get(x, y, z int) bool {
-	if x < 0 || y < 0 || z < 0 || x >= s.XLen() || y >= s.YLen() || z >= s.ZLen() {
-		return false
-	}
-	index := y*s.XLen()*s.ZLen() + z*s.XLen() + x
-	return s.Blocks[index] != 0
-}
-
-func ReadSchematic(input io.Reader) (vol *Schematic, err os.Error) {
-	var r *schematicReader
-	if r, err = newSchematicReader(input); err != nil {
+func newNbtReader(r io.Reader) (nr *nbtReader, err os.Error) {
+	var rd io.Reader
+	if rd, err = gzip.NewReader(r); err != nil {
 		return
 	}
-	vol, err = r.Parse()
+	return &nbtReader{r: bufio.NewReader(rd)}, nil
+}
+
+func (r *nbtReader) ReadString() (str string, err os.Error) {
+	var l int
+	if l, err = r.ReadShort(); err != nil {
+		return
+	}
+	data := make([]byte, l)
+	if _, err = io.ReadFull(r.r, data); err != nil {
+		return
+	}
+	return string(data), nil
+}
+
+func (r *nbtReader) ReadShort() (val int, err os.Error) {
+	buf := [2]byte{}
+	if _, err = io.ReadFull(r.r, buf[:]); err != nil {
+		return
+	}
+	val = int(buf[1]) + (int(buf[0]) << 8) // Big Endian
+	return
+}
+
+func (r *nbtReader) ReadInt() (val int, err os.Error) {
+	buf := [4]byte{}
+	if _, err = io.ReadFull(r.r, buf[:]); err != nil {
+		return
+	}
+	for i := 0; i < 4; i++ {
+		val <<= 8
+		val += int(buf[i])
+	}
+	return
+}
+
+func (r *nbtReader) ReadTagTyp() (typ byte, err os.Error) {
+	typ, err = r.r.ReadByte()
+	return
+}
+
+func (r *nbtReader) ReadTagName() (typ byte, name string, err os.Error) {
+	if typ, err = r.r.ReadByte(); err != nil {
+		return
+	}
+	if typ == tagEnd {
+		return
+	}
+	name, err = r.ReadString()
+	return
+}
+
+func (r *nbtReader) ReadByteArray() (data []byte, err os.Error) {
+	var l int
+	if l, err = r.ReadInt(); err != nil {
+		return
+	}
+	data = make([]byte, l)
+	_, err = io.ReadFull(r.r, data)
 	return
 }
